e8dd594b2fe8eafa67978d04bf6fb3d17f020ab1
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-23 00:14:47 +0100

    update Pricing class in JS

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -6,8 +6,8 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency = new Currency('', '')) {
-    this.amount = amount;
-    this.currency = currency;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   set amount(val) {
