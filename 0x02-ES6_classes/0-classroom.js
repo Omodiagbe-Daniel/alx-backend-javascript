44f6bf005d14acec5551b290d3826efd7a4c49ab
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-22 13:34:35 +0100

    Implement a class named ClassRoom in JS

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+  Implement a class named ClassRoom
+*/
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
