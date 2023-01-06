44d0a4f19f171164ff3508282760713f03d9b583
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-06 13:07:20 +0100

    used tenary operator '?' to condense an if...else statement

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,6 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  const result1 = expansion1989 === undefined ? 89 : expansion1989;
-  const result2 = expansion2019 === undefined ? 19 : expansion2019;
-
-  return initialNumber + result1 + result2;
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
 }
