df79cfad7a563925b45c96c9ae22d997d083905e
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2023-01-06 13:17:28 +0100

    return the number of arguments passed to a function using the rest parameter

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -19,4 +19,18 @@ const student2: Students = {
 const arr: Array<Students> = [student1, student2];
 
 const tb = document.createElement("table");
+const tbhead = document.createElement("thead")
 const tbBody = document.createElement("tbody");
+for (let i = 0; i < arr.length; i += 1) {
+  const row1 = document.createElement("tr");
+}
+for (let j = 0; j < arr.length; j += 1) {
+  const cell = document.createElement("td");
+  const cellText = document.createTextNode(`${arr[i].firstName}, ${arr[i].location}`);
+  cell.appendChild(cellText);
+  row1.appendChild(cell);
+}
+tbBody.appendChild(row1);
+tb.appendChild(tbBody);
+tb.setAttribute("border", "2")
+
